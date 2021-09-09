a1c9f89fcfe50bbb8f37059086ff7aac1299e17f
jzhang0107 <zhang98@bu.edu>
2021-09-09 16:40:15 -0400

    modified script for db upgrade

--- launch.sh
+++ launch.sh
@@ -3,6 +3,6 @@
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.ProductionConfig"
 flask db migrate
-python models.py db upgrade
+flask db upgrade
 #flask run --host=0.0.0.0 --port=5000
 flask run
