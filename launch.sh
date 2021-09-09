0ec2d6c3d45d59484a3fa0d0e01bbd59d13946a4
jzhang0107 <zhang98@bu.edu>
2021-09-09 16:30:29 -0400

    added models.py for dp upgrade

--- launch.sh
+++ launch.sh
@@ -3,6 +3,6 @@
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.ProductionConfig"
 flask db migrate
-flask db upgrade
+flask models.py db upgrade
 #flask run --host=0.0.0.0 --port=5000
 flask run
